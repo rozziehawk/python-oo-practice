deed33e2fac6001bbdbfcf3f09770744f8590fc9
Karl Haakonsen <cityhawk@pobox.com>
2023-10-12 10:12:24 -0400

    comment out nuisance print statement

--- wordfinder.py
+++ wordfinder.py
@@ -17,7 +17,7 @@ class WordFinder:
     
     def create_dictionary_list(self):
         """create a list from the words in a file, stripping white space (including new lines)"""
-        print("inside create_dictionary_list (base class)")
+        #print("inside create_dictionary_list (base class)")
         for line in self.file:
             #self.proces_line(line)
             #self.dictionary_list.append(line.strip()) #remove white space/newlines
@@ -31,7 +31,7 @@ class WordFinder:
 
     def ignore_line(self, line):
         ### returns true if the line being read from the file needs to be skipped
-        print("inside ignore_line in base class")
+        #print("inside ignore_line in base class")
         skip_line = False
         if (len(line) == 0 or line.isspace()):
             print(f"skipping line (base) --> {line}")
